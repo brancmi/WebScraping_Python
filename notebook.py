f1859d09e9fb6b95cf5796ba705d3ba4fa00d8ff
brancmif <76498494+brancmi@users.noreply.github.com>
2025-05-19 18:09:59 -0300

    first commit

---
+++ notebook.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class NotebookSpider(scrapy.Spider):
+    name = "notebook"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/notebooks#D[A:notebooks]"]
+
+    def parse(self, response):
+        pass
